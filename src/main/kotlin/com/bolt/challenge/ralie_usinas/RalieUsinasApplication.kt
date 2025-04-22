b8d8dc131c9bcaa6871893ac1c6c89654c24ab6a
Thiago Ribeiro <trcosta97@hotmail.com>
2025-04-22 15:12:05 -0300

    feat: projeto spring boot

---
+++ src/main/kotlin/com/bolt/challenge/ralie_usinas/RalieUsinasApplication.kt
@@ -0,0 +1,11 @@
+package com.bolt.challenge.ralie_usinas
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class RalieUsinasApplication
+
+fun main(args: Array<String>) {
+	runApplication<RalieUsinasApplication>(*args)
+}
---
+++ src/test/kotlin/com/bolt/challenge/ralie_usinas/RalieUsinasApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.bolt.challenge.ralie_usinas
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class RalieUsinasApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
